60b63517ba5f799f1bfd6e6441b13b4840102757
margie <ly.margarett@gmail.com>
2016-08-16 20:55:34 -0700

    finds winner by row

--- 1D_board/check_1D_board.rb
+++ 1D_board/check_1D_board.rb
@@ -1,7 +1,34 @@
 class Check1DBoard
+  attr_accessor :board
 
-  def initalize(board)
+  def initialize(board)
     @board = board
+  end
+
+  def message
+    if winner == "O"
+      "The winner is O"
+    elsif winner == "X"
+      "The winner is X"
+    end
+  end
+
+  def winner
+    find_by_row
+  end
+
+  def find_by_row
+    if check_row(0, 2)
+      board[0]
+    elsif check_row(3, 5)
+      board[3]
+    elsif check_row(6, 8)
+      board[6]
+    end
+  end
+
+  def check_row(x, y)
+    board[x..y].all? {|space| space == "O" || space == "X" }
   end
 
 
--- 1D_board/check_1D_board_test.rb
+++ 1D_board/check_1D_board_test.rb
@@ -10,4 +10,20 @@ class Check1DBoardTest < Minitest::Test
     @check_board = Check1DBoard.new(board)
   end
 
+  def test_it_has_a_board
+    assert_equal Array, @check_board.board.class
+  end
+
+  def test_it_checks_if_there_is_a_winner_by_row_for_O
+    @check_board.board[0..2] = "O", "O", "O"
+
+    assert_equal "The winner is O", @check_board.message
+  end
+
+  def test_it_checks_if_there_is_a_winner_by_row_for_X
+    @check_board.board[3..5] = "X", "X", "X"
+
+    assert_equal "The winner is X", @check_board.message
+  end
+
 end
